d2ced769297087dfd7d951a5b8fde23ece1ebbf2
ThiagoMonteiro <133375957+ThiagoDeLimaMonteiro@users.noreply.github.com>
2024-10-16 14:30:13 -0300

    16/10
    Aula 8
    Descobrimos como mudar o background (obrigada chat gpt)

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Qual a melhor música de brat?',
+    'nenhuma kkkk'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
